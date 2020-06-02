345e42d9b835110c29eb7e3615ca0df5bd9b7850
John Yerger <johnyerger@johns-mbp.home>
2020-06-02 15:04:59 -0400

    add money

---
+++ Money.java
@@ -0,0 +1,42 @@
+public class Money {
+
+    // variables
+    private double initialBudget = 200;
+    private double betAmount;
+    private double money; // plus minus bets / wins / loss
+
+    // constructor
+
+    public money(double betAmount){
+        this.betAmmount = betAmount;
+    }
+
+    // Budget getter
+    public double getInitialBudget() {
+        return initialBudget;
+    }
+
+    // Budget setter
+
+    // public void setInitialBudget(double initialBudget){
+    // this.initialBudger = initialBudget;
+    // }
+
+    // Bet amount getter
+
+    public double getBetAmount() {
+        return betAmount;
+    }
+
+    // Bet amount Setter
+
+    public void setBetAmount(double betAmount) {
+        this.betAmount = betAmount;
+    }
+
+    public double getMoney() {
+        // initial budget minus bet amount / wins / loss
+
+    }
+
+}
